91ee0abfdc370ae5e6f473e9eb0b8519767f1fb3
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-15 12:45:10 +0300

    add tailwindcss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
+// import { Link } from "react-router-dom";
+
 const Footer = () => {
     return ( 
         <>
-            Footer here...
+            Footer Here...
         </>
      );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <>
-            Navbar here...
+            <nav className="bg-blue-400 p-6">
+                <div className="flex md:justify-around justify-between">
+                    <div>
+                        Doctor's Appointment Booking
+                    </div>
+
+                    <div>
+                        <ul className="md:flex">
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-2'>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
      );
 }
