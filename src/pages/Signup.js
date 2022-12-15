1191a16027a081acf07805b9c6e2d59125531352
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-12-15 14:35:04 +0300

    creating a user function

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,14 @@
-const Signup = () => {
-    return ( 
-        <>
-            Signup here...
-        </>
-     );
-}
- 
-export default Signup;
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+function SignUp({ setUser }) {
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
+    const [user, setUserData] = useState({
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+      phone: "",
+      category: "doctor",
+    });
