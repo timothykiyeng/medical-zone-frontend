ba50f8acfe93bf3e6ba1af700e9d8c13adbfd784
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-16 09:19:03 +0300

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,45 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { BsGithub, BsInstagram, BsTwitter, BsLinkedin, BsFacebook } from "react-icons/bs"
+
 
 const Footer = () => {
     return ( 
         <>
-            Footer Here...
+            <footer className="bg-pink-400 md:py-12 py-6">
+                <div className="md:flex md:justify-around justify-center items-center  text-center md:text-base text-sm">
+                    <div>
+                        <h1>Medibook</h1>
+                    </div>
+
+                    <div className="md:my-0 my-2">
+                        <p>Copyright &copy; 2022 |  
+                            <Link to='/'> 
+                               | Medibook
+                            </Link>
+                        </p>
+                    </div>
+
+                    <div className="flex justify-center text-lg text-gray-900">
+                        <Link to='/'  className="mx-2">
+                            <BsGithub />
+                        </Link>
+                        <Link to='/'  className="mx-2">
+                            <BsFacebook />
+                        </Link>
+                        <Link to='/'  className="mx-2">
+                            <BsInstagram />
+                        </Link>
+                        <Link to='/'  className="mx-2">
+                            <BsTwitter />
+                        </Link>
+                        <Link to='/'  className="mx-2">
+                            <BsLinkedin />
+                        </Link>
+                    </div>
+                </div>
+            </footer>
         </>
-     );
+    );
 }
  
 export default Footer;
