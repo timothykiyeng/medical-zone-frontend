2a0dc0f18a9774f0ace266a059f554991a9f7f9d
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-10 09:19:51 +0300

    add nabar routes style

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,6 @@ const Hero = () => {
                 >
                 <div className="flex flex-col justify-center items-center lg:my-24 md:my-16 my-6 lg:mx-32 md:mx-16 mx-8">
                     <h2 className="font-medium md:text-lg text-base text-gray-500">
-                        Home - 
                         <span className="text-pink-500 mx-2">Home</span>
                     </h2>
                     <h1 className='mb-4 md:text-5xl text-3xl font-bold uppercase text-sky-600 md:mt-8 mt-4 text-center'>We Offer Quality Treatment But Only God Heals</h1>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,56 @@
 import { Link } from "react-router-dom";
-import { useRef } from "react";
-import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 
 const Navbar = () => {
-    const menuRef = useRef()
+    const [open, setOpen] = useState(false)
 
-    const showMenu = () => {
-        menuRef.current.classList.toggle("hidden")
-    }
+    const navlinks = [
+        {path: '/about', name: 'About'},
+        {path: '/contact', name: 'Contact'}
+    ]
 
     return ( 
-        <div className="bg-sky-400 py-2 px-6">
-            <nav className='md:py-6 py-4'>
-                <div className="navbar justify-between items-center " >
-                    <div className='flex items-center md:justify-around justify-between'>
+        <>
+            <nav className='lg:px-64 md:px-40 px-8 md:py-6 py-4 top-0 left-0 sticky z-[100] bg-sky-400 opacity-100 shadow-xl'>
+                <div className='md:flex items-center justify-between'>
+                    <div className="flex justify-between items-center">
                         <Link to='/' className='md:text-3xl text-xl uppercase font-semibold text-white'>
                             Medical Zone
                         </Link>
                         
-                        <ul className="md:flex items-center md:text-lg font-medium uppercase hidden">
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/about'>About</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 my-2'>
-                                <Link to='/contact'>Contact</Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 md:py-0 py-3'>
-                                <Link to='/login' className="py-2 px-4 rounded-lg bg-white">
-                                    Login
-                                </Link>
-                            </li>
-                            <li className='md:mx-8 md:my-0 md:py-0 py-3'>
-                                <Link to='/signup' className="py-2 px-4 rounded-lg bg-pink-500 text-white">
-                                    Signup
-                                </Link>
-                            </li>
-                            
-                        </ul>
-
-                        <button onClick={showMenu} className="md:hidden">
-                            <FaBars />
+                        <button onClick={() => setOpen((prev) => !prev)} className="md:hidden text-xl text-gray-800">
+                            {open ? <FaTimes/> : <FaBars/>}
                         </button>
+                    </div>
+                    
+                    <div className={`${open ? "block" : "hidden"} md:flex`}>
+                        <ul className="md:flex items-center md:text-lg font-medium uppercase">
+                            {navlinks.map((navlink) => (
+                                <li className='md:mx-8 md:my-0 my-4 hover:text-pink-100'>
+                                    <Link to={navlink.path}>{navlink.name}</Link>
+                                </li>
+                            ))}
+                            <li className='md:mx-8 md:my-0 my-4'>
+                                <Link 
+                                    className="bg-pink-100 px-4 py-2 hover:bg-pink-500 hover:text-white duration-500 rounded-md" 
+                                    to='/login'>
+                                        Login
+                                    </Link>
+                            </li>
+                            <li className='md:mx-8 md:my-0 my-4'>
+                                <Link 
+                                    className="text-white bg-pink-500 px-4 py-2 hover:text-pink-500 hover:bg-white duration-500 rounded-md" 
+                                    to='/signup'>
+                                        Signup
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
-        </div>
+        </>
     );
 }
  
