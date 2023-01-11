792b2b88185f6548d22fc4aeada390593a63bb2c
Zakaria <najkacade100@gmail.com>
2023-01-11 13:17:56 +0300

    Add about.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import AboutBanner from '../assets/about.jpg'
+import "./About.css" 
 
 const About = () => {
     return ( 
@@ -14,12 +15,27 @@ const About = () => {
                     >
                     <div className="flex flex-col justify-center items-center lg:my-24 md:my-16 my-6 md:mx-0 mx-4">
                         <h2 className="font-medium md:text-lg text-base text-white">
-                            Home - 
+                            
                             <span className="text-pink-500 mx-2">About Us</span>
                         </h2>
-                        <h1 className='md:text-5xl text-3xl font-bold uppercase text-gray-800 md:mt-8 mt-4 text-center'>Your Wellbeing & Satisfaction is Our Joy</h1>
+                        <h1 className='md:text-5xl text-3xl font-bold uppercase text-sky-400 md:mt-8 mt-4 text-center'>Your Wellbeing & Satisfaction is Our Joy</h1>
                     </div>
                 </div>
+                <div class="flex flex-col justify-center items-center lg:my-24 md:my-16 my-6 md:mx-0 mx-4">
+                    <h2 className="font-medium md:text-lg text-base text-white">
+
+                        <span className="text-pink-500 mx-2">Our Values</span>
+                    </h2>
+                    <h1 className='md:text-1xl font-medium text-2xl text-black md:mt-8 mt-4 text-center'>We're an ambitious and smart team with a shared mission</h1>
+
+                    <div className="flex flex-col justify-center items-left left lg:my-24 md:my-16 my-6 md:mx-0 mx-4">
+                    <h2 className="font-medium md:text-lg text-base text-white text-align: left">
+
+                    <span className="text-black mx-2">Our team</span>
+                    </h2>
+                </div>
+                </div>
+              
 
 
             </div>
