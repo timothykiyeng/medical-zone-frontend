02c63fd9de516eb1d964ba0e39140ccdfbc2dd9b
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2023-01-11 23:24:13 +0300

    Add Login component for patient sign up and doctor login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,2 +1,22 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+const Login = ({ setUser }) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [docLogin, setDocLogin] = useState(true);
+  const [patLogin, setPatLogin] = useState(false);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    birthdate: "",
+    age: "",
+    doc: false,
+  });
