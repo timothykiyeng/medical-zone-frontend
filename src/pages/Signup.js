393366348a9e7417a80d8f319e8e8af944d7b292
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2023-01-12 19:37:33 +0300

    modifying the code to suit the updated functions

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,128 +1,137 @@
-// import React, { useState } from "react";
-// import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Signup.css";
+const Signup = ({ setUser }) => {
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState([]);
+  const [gender, setGender] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    birthdate: "",
+    age: "",
+    doc: false,
+  });
 
-// function SignUp({ setUser }) {
-//   const navigate = useNavigate();
-//   const [errors, setErrors] = useState([]);
-//   const [user, setUserData] = useState({
-//     name: "",
-//     email: "",
-//     password: "",
-//     password_confirmation: "",
-//     phone: "",
-//     category: "doctor",
-//   });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupData((signupData) => ({ ...signupData, [name]: value }));
+  };
 
-//   // Keep track of the changes made to the form
-//   function handleChanges(e) {
-//     console.log(e.target.name);
-//     setUserData({ ...user, [e.target.name]: e.target.value });
-//   }
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    if (confirmPassword !== signupData.password) {
+      alert("Passwords dont' match!");
+    }
+    fetch("http://127.0.0.1:3000/patients", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(signupData),
+    }).then((r) => {
+      if (r.ok) {
+        r.json()
+          .then((user) => {
+            setUser(user);
+          })
+          .then(() => navigate("/"));
+      } else {
+        r.json().then((json) => setErrors(json.errors));
+      }
+    });
+  };
 
-//   // Vallidate the user inputs and check if any are emptty
-//   function validateUserInput(e) {
-//     if (
-//       user.name === "" ||
-//       user.email === "" ||
-//       user.phone === "" ||
-//       user.password === "" ||
-//       user.password_confirmation === ""
-//     ) {
-//       alert("All fields are required");
-//     } else {
-//       createUser(e);
-//     }
-//   }
+  return (
+    <div className="signup">
+      <h1>Get started</h1>
+      <form onSubmit={handleSignUpSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={signupData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={signupData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={signupData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          name="birthdate"
+          placeholder="Birthdate"
+          value={signupData.birthdate}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          value={signupData.age}
+          onChange={handleChange}
+        />
+        <label>
+          Doctor
+          <input
+            type="checkbox"
+            name="doc"
+            checked={signupData.doc}
+            onChange={(e) =>
+              handleChange({ target: { name: "doc", value: e.target.checked } })
+            }
+          />{" "}
+        </label>
+        <div className="gender-radio">
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="male"
+            onChange={(e) => setGender(e.target.value)}
+          />
+          <label htmlFor="male">Male</label>
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="female"
+            onChange={(e) => setGender(e.target.value)}
+          />
+          <label htmlFor="female">Female</label>
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+      {errors.length > 0 ? (
+        <ul>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
 
-//   //add a new user to the database
-//   function createUser(event) {
-//     event.preventDefault();
-//     fetch("/users/", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(user),
-//     }).then((response) => {
-//       if (response.ok) {
-//         response.json().then((userData) => {
-//           setUser(userData);
-//           userData.category === "doctor"
-//             ? navigate("/")
-//             : navigate("/dashboard");
-//           alert("Created account successfully");
-//         });
-//       } else {
-//         alert("Failed to create account");
-//         response.json().then((error) => setErrors(error.errors));
-//       }
-//     });
-//   }
-//   return (
-//     <div className="signUpForm">
-//       <form onSubmit={validateUserInput}>
-//         <h2>
-//           Fill in the details <br />
-//           to create your account
-//         </h2>
-//         <input
-//           name="name"
-//           type={"text"}
-//           placeholder="Full Name"
-//           value={user.name}
-//           onChange={handleChanges}
-//         />{" "}
-//         <br />
-//         <input
-//           name="email"
-//           type={"email"}
-//           placeholder="Email"
-//           value={user.email}
-//           onChange={handleChanges}
-//         />
-//         <br />
-//         <input
-//           name="phone"
-//           type={"number"}
-//           placeholder="Phone"
-//           value={user.phone}
-//           onChange={handleChanges}
-//         />
-//         <br />
-//         <select name="category" value={user.category} onChange={handleChanges}>
-//           <option value="doctor">Doctor</option>
-//           <option value="patient">Patient</option>
-//         </select>
-//         <br />
-//         <input
-//           name="password"
-//           type={"password"}
-//           placeholder="Password"
-//           value={user.password}
-//           onChange={handleChanges}
-//           autoComplete="on"
-//         />
-//         <br />
-//         <input
-//           name="password_confirmation"
-//           type={"password"}
-//           placeholder="Password Confirmation"
-//           value={user.password_confirmation}
-//           onChange={handleChanges}
-//         />
-//         <br />
-//         <input type={"submit"} id="submit" value="Create Account" />
-//         <p>
-//           Already have an account?{" "}
-//           <span>
-//             <NavLink to={"/login"} className="sign">
-//               Sign In
-//             </NavLink>
-//           </span>
-//           !
-//         </p>
-//       </form>
-//     </div>
-//   );
-// }
-// export default SignUp;
+export default Signup;
