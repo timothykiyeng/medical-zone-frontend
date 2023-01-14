51f7e05356cc6a02eb31b63c3683273ccc223b58
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-14 09:43:47 +0300

    add login-page css-code refactoring

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,248 @@
-const Login = () => {
-    return ( 
-        <>
-            Login here...
-        </>
-     );
-}
- 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Login.css";
+
+const Login = ({ setUser }) => {
+  const navigate = useNavigate();
+  const [error, setError] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [docLogin, setDocLogin] = useState(true);
+  const [patLogin, setPatLogin] = useState(false);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    birthdate: "",
+    age: "",
+    doc: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((loginData) => ({ ...loginData, [name]: value }));
+  };
+  const handleSignUpChange = (e) => {
+    const { name, value } = e.target;
+    setSignupData((signupData) => ({ ...signupData, [name]: value }));
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    setError([]);
+    fetch(
+      docLogin
+        ? "http://127.0.0.1:3000/doclogin"
+        : "http://127.0.0.1:3000/patientlogin",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(loginData),
+      }
+    ).then((r) => {
+      if (r.ok) {
+        r.json()
+          .then((user) => {
+            setUser(user);
+          })
+          .then(() => navigate("/"));
+      } else {
+        r.json().then((json) => setError(json.error));
+      }
+    });
+  };
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    if (confirmPassword !== signupData.password) {
+      alert("Passwords dont' match!");
+    }
+    fetch("/patients", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(signupData),
+    }).then((r) => {
+      if (r.ok) {
+        r.json()
+          .then((user) => {
+            setUser(user);
+          })
+          .then(() => navigate("/"));
+      } else {
+        r.json().then((json) => setErrors(json.errors));
+      }
+    });
+  };
+
+  const handleDocClick = () => {
+    setDocLogin(true);
+    setPatLogin(false);
+    setLoginData({ email: "", password: "" });
+    setError([]);
+  };
+  const handlePatClick = () => {
+    setDocLogin(false);
+    setPatLogin(true);
+    setLoginData({ email: "", password: "" });
+    setError([]);
+  };
+  const handleSignupClick = () => {
+    setDocLogin(false);
+    setPatLogin(false);
+    setLoginData({ name: "", email: "", password: "", birthdate: "" });
+    setErrors([]);
+  };
+
+  return (
+    <div className="login min-h-[80vh] flex flex-col items-center justify-center">
+      <figure className="login-form-box ">
+        <div className="selector ">
+          <button
+            className={docLogin ? "active" : null}
+            onClick={handleDocClick}
+          >
+            Doctor
+          </button>
+          <button
+            className={patLogin ? "active" : null}
+            onClick={handlePatClick}
+          >
+            Patient
+          </button>
+          
+        </div>
+        
+        <div className="flex justify-center items-center md:mt-16 mt-8">
+          {docLogin || patLogin ? (
+            <form className="input-group bg-sky-400 md:py-16 py-8 md:px-12 px-6 rounded-lg" onSubmit={handleLoginSubmit}>
+              <div>
+                <label className="text-left ">
+                  {docLogin ? "Doctor's " : "Patient "} Email:
+                </label><br/>
+                <input
+                  className="input mt-2"
+                  type="text"
+                  name="email"
+                  id="email"
+                  onChange={handleChange}
+                  value={loginData.email}
+                ></input>
+              </div>
+              <div className="mt-4">
+                <label className="input-label">Password:</label><br/>
+                <input
+                  className="input mt-2"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={handleChange}
+                  value={loginData.password}
+                ></input>
+              </div>
+
+              {error ? <p className="error">{error}</p> : null}
+
+              
+              <button onClick={handleLoginSubmit} value="Login" className="btn">
+                Login
+              </button>
+
+            </form>
+            ) : (
+              <form className="input-group">
+                <div>
+                  <label className="input-label">Name:</label>
+                  <input
+                    className="input"
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={handleSignUpChange}
+                    value={signupData.name}
+                  ></input>
+                </div>
+                
+                <div>
+                  <label className="input-label">Email:</label>
+                  <input
+                    className="input"
+                    type="text"
+                    name="email"
+                    id="email"
+                    onChange={handleSignUpChange}
+                    value={signupData.email}
+                ></input>
+                </div>
+                
+                <div>
+                  <label className="input-label">Age:</label>
+                  <input
+                    className="input"
+                    type="text"
+                    name="age"
+                    id="age"
+                    onChange={handleSignUpChange}
+                    value={signupData.age}
+                  ></input>
+                </div>
+                
+                <div>
+                  <label className="input-label">Birthdate:</label>
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="mm/dd/yyyy"
+                    name="birthdate"
+                    id="birthdate"
+                    onChange={handleSignUpChange}
+                    value={signupData.birthdate}
+                  ></input>
+                </div>
+
+                <div>
+                  <label className="input-label">Password:</label>
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={handleSignUpChange}
+                    value={signupData.password}
+                  ></input>
+                </div>
+                
+                <div>
+                  <label className="input-label">Confirm Password:</label>
+                  <input
+                    className="input"
+                    type="password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  ></input>
+                </div>
+
+                {errors ? errors.map((e) => <p className="error">{e}</p>) : null}
+
+                <button onClick={handleSignUpSubmit} className="button">
+                  Sign Up
+                </button>
+            </form>
+          )}
+        </div>
+
+      </figure>
+    </div>
+  );
+};
+
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,119 @@
-const Signup = () => {
-    return ( 
-        <>
-            Signup here...
-        </>
-     );
-}
- 
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Signup.css";
+const Signup = ({ setUser }) => {
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState([]);
+  const [gender, setGender] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    birthdate: "",
+    age: "",
+    doc: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupData((signupData) => ({ ...signupData, [name]: value }));
+  };
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    if (confirmPassword !== signupData.password) {
+      alert("Passwords dont' match!");
+    }
+    fetch("http://127.0.0.1:3000/patients", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(signupData),
+    }).then((r) => {
+      if (r.ok) {
+        r.json()
+          .then((user) => {
+            setUser(user);
+          })
+          .then(() => navigate("/"));
+      } else {
+        r.json().then((json) => setErrors(json.errors));
+      }
+    });
+  };
+
+  return (
+    <div className="signup">
+      <h1>Get started</h1>
+      <form onSubmit={handleSignUpSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={signupData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={signupData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={signupData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          name="birthdate"
+          placeholder="dd/mm/yyyy"
+          value={signupData.birthdate}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          value={signupData.age}
+          onChange={handleChange}
+        />
+        <div className="gender-select">
+          <select
+            id="gender-select"
+            placeholder="Gender"
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+
+        <button type="submit">Sign Up</button>
+      </form>
+      {errors.length > 0 ? (
+        <ul>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
 export default Signup;
