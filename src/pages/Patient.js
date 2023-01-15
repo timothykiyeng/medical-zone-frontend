c714bf396d105f67c055030b21a9e71ce2534755
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2023-01-15 11:55:10 +0300

    moving the logout button

--- src/pages/Patient.js
+++ src/pages/Patient.js
@@ -11,12 +11,12 @@ const Patient = ({ setUser }) => {
   return (
     <>
       <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <div>
         <h1 className="header text-gray-700 text-center md:my-8 my-4">
           Patient History
         </h1>
-        <div>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
       </div>
     </>
   );
