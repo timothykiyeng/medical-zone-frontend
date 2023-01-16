e475ae8f5e62d44b0eddd51bb96c3f3d82024510
saidsoud72 <said.omar@student.moringaschool.com>
2023-01-16 21:46:48 +0300

    abou header

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,8 @@
 import React from "react";
 const About = () => (
-  <div></div>
+  <div>
+    <h2 className="populr">About us</h2>
+    <p className="landing3">
+      Looking for an easy and convenient way to book appointments with your
+      doctor?
+      <br />
