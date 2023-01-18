4dad7f7af418e4c73f4ac552492ea3db3fe746af
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-18 09:13:53 +0300

    add team && team & values testimonials

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,5 +1,6 @@
 import data from "../data/data.json"
 import { motion } from "framer-motion";
+import { Fade } from "react-reveal";
 
 function OurTeam  () {
    
@@ -7,21 +8,23 @@ function OurTeam  () {
           <>
             <div className='team-main-container md:my-32 my-16 md:mx-64 mx-8'>
               <h1 className="header text-center">Our Team</h1>
-              <div className='team-container grid grid-cols-2 md:grid-cols-4'>
-                {data.ourteam.map((team) => (
-                  <motion.div 
-                    whileInView={{ opacity: 1 }}
-                    whileHover={{  y: -16 }}
-                    transition={{type: 'spring', stiffness: 120}}
-                    key={team.id} 
-                    className="flex flex-col teams-center rounded-3xl md:mx-8 mx-0 p-8 md:my-4 my-8 bg-white shadow-md hover:shadow-lg hover:bg-sky-100 pb-6 ">
-                    <img className="rounded-3xl" src={team.image} alt=""/>
-                    <div className="md:pt-6 pt-4 p-4 text-center">
-                        <p className="text-sky-500 text-xl font-semibold uppercase px-2">{team.name}</p>
-                        <p className="text-gray-700 font-light md:text-lg text-base">{team.remarks}</p>
-                    </div>
-                  </motion.div>
-                ))}
+              <div className='team-container grid grid-cols-2 md:grid-cols-4 gap-4 md:mt-8 mt-4'>
+                <Fade right>
+                  {data.ourteam.map((team) => (
+                    <motion.div 
+                      whileInView={{ opacity: 1 }}
+                      whileHover={{  y: -16 }}
+                      transition={{type: 'spring', stiffness: 120}}
+                      key={team.id} 
+                      className="flex flex-col teams-center rounded-3xl md:mx-8 mx-0 md:p-8 p-4 md:my-4 my-8 bg-white shadow-md hover:shadow-lg hover:bg-green-100 pb-6 ">
+                      <img className="md:rounded-3xl rounded-2xl" src={team.image} alt=""/>
+                      <div className="md:pt-6 pt-4 md:p-4 p-2 text-center">
+                          <p className="text-green-500 text-xl font-semibold uppercase px-2">{team.name}</p>
+                          <p className="text-gray-700 font-light md:text-lg text-base">{team.remarks}</p>
+                      </div>
+                    </motion.div>
+                  ))}
+                </Fade>
               </div>
             </div>
           </>
