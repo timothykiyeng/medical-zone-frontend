4c164e7980b07ee8e4bdc0e78d9f2fa3511fc0ce
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-18 08:05:48 +0300

    add inquiries section

--- src/components/Inquiries.js
+++ src/components/Inquiries.js
@@ -4,16 +4,29 @@ import getInTouch from "../assets/get-in-touch.jpg"
 function Inquiry () {
     return (
         <>
-            <div className="bg-cover object-cover h-64 w-full" style={{ backgroundImage: `url(${getInTouch})` }}>
-            <h2>
-                Keep us updated for any inuiries
-            </h2>
-            
+            <div>
+                <div className="" 
+                    style={{
+                            backgroundImage: `url(${getInTouch})`, 
+                            backgroundSize: `cover`,
+                            backgroundRepeat: `no-repeat`,
+                            paddingTop: `100px`, 
+                            paddingBottom: `100px` 
+                        }}
+                    >
+                    <div className="flex md:flex-row flex-col justify-center items-center md:my-8 md:mx-0 mx-8">
+                        <h2 className="header md:text-5xl text-gray-100 text-center md:mb-0 mb-8">
+                            Keep us update for any inquiries
+                        </h2>
+                        <Link 
+                            to="/contact"
+                            className="btn text-white bg-sky-500 uppercase font-medium md:mx-8 mx-4">
+                            Reach out
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <Link to="/contact"
-                className="btn text-white bg-blue-400 uppercase font-medium md:mx-8 mx-4 position-absolute right-0 top-0">
-                Reach out
-            </Link>
+            
         </>
     )
 }
