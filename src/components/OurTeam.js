deda9e996dcce2cea869e01b2a48a27f41ed5a54
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-18 09:03:40 +0300

    add our team

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,35 +1,30 @@
-import { useEffect, useState } from "react";
+import data from "../data/data.json"
+import { motion } from "framer-motion";
 
 function OurTeam  () {
-    const [doctors, setDoctors] = useState([]);
-    
-  
-    useEffect(() => {
-      fetch(`http://localhost:3000/doctors`)
-        .then((response) => response.json())
-        .then((d) => setDoctors(d));
-    }, []);
+   
     return ( 
-        <>
-        <div className='team-main-container'>
-        <h1>Meet The Team</h1>
-        <div className='team-container'>
-          {doctors ? doctors.map((doc) => {
-              <div className='team-card'>
-                <img
-                  src={doc.image}
-                  alt='Image by <a href="https://www.freepik.com/free-photo/front-view-nurse-wearing-stethoscope_32486202.htm#page=2&query=black%20woman%20doctor&position=7&from_view=keyword">Freepik</a>'
-                />
-                <h2>
-                  MD. {doc.name}
-                </h2>
-                <p>{doc.doctor.bio} Specialist</p>
+          <>
+            <div className='team-main-container md:my-32 my-16 md:mx-64 mx-8'>
+              <h1 className="header text-center">Our Team</h1>
+              <div className='team-container grid grid-cols-2 md:grid-cols-4'>
+                {data.ourteam.map((team) => (
+                  <motion.div 
+                    whileInView={{ opacity: 1 }}
+                    whileHover={{  y: -16 }}
+                    transition={{type: 'spring', stiffness: 120}}
+                    key={team.id} 
+                    className="flex flex-col teams-center rounded-3xl md:mx-8 mx-0 p-8 md:my-4 my-8 bg-white shadow-md hover:shadow-lg hover:bg-sky-100 pb-6 ">
+                    <img className="rounded-3xl" src={team.image} alt=""/>
+                    <div className="md:pt-6 pt-4 p-4 text-center">
+                        <p className="text-sky-500 text-xl font-semibold uppercase px-2">{team.name}</p>
+                        <p className="text-gray-700 font-light md:text-lg text-base">{team.remarks}</p>
+                    </div>
+                  </motion.div>
+                ))}
               </div>
-            
-          }) : <h1>Our Practitioners will appear here</h1>}
-        </div>
-      </div>
- </>
+            </div>
+          </>
      );
 }
  
