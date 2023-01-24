a74d5c62c000150b42fb924d83f8104dd8c0184e
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2023-01-24 09:34:49 +0300

    style doctor's profile

--- src/components/DoctorProfile.js
+++ src/components/DoctorProfile.js
@@ -5,11 +5,15 @@ import React from 'react';
 const DoctorProfile = ({ doc }) => {
 
   return (
-    <div>
-      <h1>{`Dr. ${doc[0].name}, ${doc[0].title}`}</h1>
-      <img src = {doc[0].image_url} alt='img'/>
-      <h3>{doc[0].bio}</h3>
-      <h5>Contact: {doc[0].email}</h5>
+    <div className='md:flex md:justify-center md:space-x-8 xl:mx-32 mx-8 md:my-16 my-8 min-h-[60vh]'>
+      <img className='md:w-[50%] rounded-xl' src = {doc[0].image_url} alt='img'/>
+      <div className='mt-8'>
+        <h1 className='font-medium text-gray-800 uppercase'>{`Dr. ${doc[0].name}, ${doc[0].title}`}</h1>
+        <h3 className='my-2 font-light'>{doc[0].bio}</h3>
+        <h5 className='font-medium text-gray-800 uppercase '>Contact: 
+          <span className='mx-1 text-sky-500 lowercase'>{doc[0].email}</span>
+        </h5>
+      </div>
     </div>
   )
 }
